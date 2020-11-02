24882ad3800816093a8d17ae555f04612faa5129
Francesco <francescofacca@gmail.com>
2020-11-02 23:04:04 +0000

    Done  some refactoring for oystercard_spec

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,22 +3,29 @@ require "oystercard"
 describe Oystercard do
   subject(:oystercard) {described_class.new}
 
-  it "should return balance of £0 by default" do
+  it "returns balance of £0 by default" do
     expect(oystercard.balance).to eq 0
   end
 
-  it "should return a balance of £10 when top up £10" do
-    expect{ oystercard.top_up(10) }.to change{ oystercard.balance }.by(10)
+  describe "#top_up" do
+    context "When added money" do
+      it "returns balance of £10" do
+        expect{ oystercard.top_up(10) }.to change{ oystercard.balance }.by(10)
+      end
+
+      it "raises an error if takes balance over £90" do
+        maximum_balance = Oystercard::MAXIMUM_BALANCE
+        oystercard.top_up(maximum_balance)
+        expect{ oystercard.top_up(1) }.to raise_error "Maximum balance if £#{maximum_balance} exceeded"
+      end
+    end
   end
 
-  it "should throw an error when top_up takes balance over £90" do
-    maximum_balance = Oystercard::MAXIMUM_BALANCE
-    oystercard.top_up(maximum_balance)
-    expect{ oystercard.top_up(1) }.to raise_error "Maximum balance if £#{maximum_balance} exceeded"
+  describe "#deduct" do
+    context "When deduct money" do
+      it "reduces balance by £5" do
+        expect{ oystercard.deduct(5) }.to change{ oystercard.balance }.by(-5)
+      end
+    end
   end
-
-  it "should reduce balance by £5 when deduct £5" do
-    expect{ oystercard.deduct(5) }.to change{ oystercard.balance }.by(-5)
-  end
-
 end
