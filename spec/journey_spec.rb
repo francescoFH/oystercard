36debbaa328db38d200e5e0b2edefa98f3e1e8f1
Francesco <francescofacca@gmail.com>
2020-11-05 00:06:01 +0000

    Done some refactoring

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -3,31 +3,37 @@ require 'journey'
 describe Journey do
 let (:station) { double :station}
 
-  it "is nil by default" do
-    expect(subject.enter_station).to eq(nil)
+  describe "#initialize" do
+    it "stores the enter station" do
+      subject.enter_station = station
+      expect(subject.enter_station).to eq(station)
+    end
+
+    it "stores the exit station" do
+      subject.exit_station = station
+      expect(subject.exit_station).to eq(station)
+    end
+
+    context "defaults" do
+      it "has default nil for enter_station" do
+        expect(subject.enter_station).to eq(nil)
+      end
+
+      it "has default nil for exit_station" do
+        expect(subject.exit_station).to eq(nil)
+      end
+    end
   end
 
-  it "stores the enter station" do
-    subject.enter_station = station
-    expect(subject.enter_station).to eq(station)
-  end
+  describe "#fare" do
+    it "returns default fare if end and start aren't nil" do
+      subject.enter_station = station
+      subject.exit_station = station
+      expect(subject.fare).to eq Oystercard::FARE
+    end
 
-  it "is nil by default" do
-    expect(subject.exit_station).to eq(nil)
-  end
-
-  it "stores the enter station" do
-    subject.exit_station = station
-    expect(subject.exit_station).to eq(station)
-  end
-
-  it "returns default fare if end and start aren't nil" do
-    subject.enter_station = station
-    subject.exit_station = station
-    expect(subject.fare).to eq Oystercard::FARE
-  end
-
-  it "returns penalty fare if enter or exit are nil" do
-    expect(subject.fare).to eq Journey::PENALTY_FARE
+    it "returns penalty fare if enter or exit are nil" do
+      expect(subject.fare).to eq Journey::PENALTY_FARE
+    end
   end
 end
